11aa2bc7a98bb44751a3328917a81da48929606c
Tiiawss <tiia.nylen@gmail.com>
2022-11-13 19:28:01 +0200

    12

--- viikko2/verkkokauppa-1/src/kirjanpito.py
+++ viikko2/verkkokauppa-1/src/kirjanpito.py
@@ -1,9 +1,9 @@
 class Kirjanpito:
-    __instanssi = None
-
-
     def __init__(self):
         self.tapahtumat = []
 
     def lisaa_tapahtuma(self, tapahtuma):
         self.tapahtumat.append(tapahtuma)
+
+
+kirjanpito = Kirjanpito()
--- viikko2/verkkokauppa-1/src/pankki.py
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -1,14 +1,17 @@
-from kirjanpito import Kirjanpito
+from kirjanpito import kirjanpito as default_kirjanpito
 
 
 class Pankki:
-    
-    def __init__(self, kirjanpito):
+    def __init__(self, kirjanpito=default_kirjanpito):
         self._kirjanpito = kirjanpito
 
     def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
         self._kirjanpito.lisaa_tapahtuma(
             f"tilisiirto: tililtä {tililta} tilille {tilille} viite {viitenumero} summa {summa}e"
         )
+
         
         return True
+
+
+pankki = Pankki()
