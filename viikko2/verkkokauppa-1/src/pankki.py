2ebaa4b63d6cf438cc6a0976f4fbcd68aa1679f1
Tiiawss <58226295+Tiiawss@users.noreply.github.com>
2022-11-13 17:43:13 +0200

    Update pankki.py

--- viikko2/verkkokauppa-1/src/pankki.py
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -2,22 +2,13 @@ from kirjanpito import Kirjanpito
 
 
 class Pankki:
-    __instanssi = None
-
-    @staticmethod
-    def get_instance():
-        if not Pankki.__instanssi:
-            Pankki.__instanssi = Pankki()
-
-        return Pankki.__instanssi
-
-    def __init__(self):
-        self._kirjanpito = Kirjanpito.get_instance()
+    
+    def __init__(self, kirjanpito):
+        self._kirjanpito = kirjanpito
 
     def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
         self._kirjanpito.lisaa_tapahtuma(
             f"tilisiirto: tililtä {tililta} tilille {tilille} viite {viitenumero} summa {summa}e"
         )
-
-        # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        
         return True
