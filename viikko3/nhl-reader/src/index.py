b0d986e3e231a765c191853e6478e1c6871c1569
Tiiawss <tiia.nylen@gmail.com>
2022-11-18 12:39:14 +0200

    tehtävä 3

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,33 +1,26 @@
+from player_stats import PlayerStats
+from player_reader import PlayerReader
 import requests
 from datetime import datetime
 from player import Player
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
-    response = requests.get(url).json()
+    
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     
     
 
-    players = []
-
-    for player_dict in response:
-        player = Player(
-            player_dict['name'],
-            player_dict['nationality'],
-            player_dict['team'],
-            player_dict['goals'],
-            player_dict['assists']
-        )
-
-        players.append(player)
-    
+   
     print(f"Players from FIN {datetime.now()}")
     print(" ")
     
 
     for player in players:
-        if "FIN" in player.nationality:
-            print(player)
+        print(player)
+        
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,10 +1,10 @@
 class Player:
-    def __init__(self, name, nationality, team, goals, assists):
+    def __init__(self, name, team, goals, assists, nationality):
         self.name = name
-        self.nationality = nationality
         self.team = team
         self.goals = goals
         self.assists = assists
+        self.nationality = nationality
     
     def __str__(self):
         return f"{self.name:20} {self.team} {str(self.goals):2} + {str(self.assists):2} = {self.goals + self.assists}"
