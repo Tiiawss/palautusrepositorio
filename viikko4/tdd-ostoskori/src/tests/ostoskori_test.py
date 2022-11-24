bcd6909ab13d99ceef5ca6051010094ca59f4db3
Tiiawss <tiia.nylen@gmail.com>
2022-11-24 17:33:36 +0200

    Yhden tuotteen lisäämisen jälkeen ostoskori sisältää yhden ostoksen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -39,9 +39,15 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
-        
+
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteiden_hinta(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 6)
+    
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
