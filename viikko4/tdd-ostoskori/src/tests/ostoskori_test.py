1336aab241fcbeccd8996bc7c4c88722d32d9f1e
Tiiawss <tiia.nylen@gmail.com>
2022-11-24 17:36:25 +0200

    kaksi eri tuotetta kaksi eri oliota ostoskorissa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -59,4 +59,11 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
-    
+        
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_kaksi_ostosoliota(self):
+        maito = Tuote("Maito", 3)
+        suklaa = Tuote("Muna", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(suklaa)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
